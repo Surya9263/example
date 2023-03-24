e1fbb8bede1070aa162d4ccac2db292e721106ac
MY PC <suryasrivastava724@gmail.com>
2023-03-24 17:41:27 +0530

    added recipe page

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,3 @@
-async function getRecipe(){
-    const res=await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=c")
-    const data=await res.json()
-    return data
-}
-
 async function getCategories(){
     const res=await fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     const data=await res.json()
@@ -36,3 +30,44 @@ function showCategories(){
     })
 }
 
+
+async function getMealByQuery(){
+    let query=document.getElementById("search").value
+    console.log(query);
+    const res=await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+    const data=await res.json()
+    console.log(data.meals);
+    
+    if(!query){
+        document.getElementById("searched-items").innerHTML=null
+        return
+    }
+
+    document.getElementById("searched-items").innerHTML=null
+
+    data.meals.map((e)=>{
+        
+        let div=document.createElement("div")
+        let img=document.createElement("img")
+        img.src=e.strMealThumb
+        let h3=document.createElement("h3")
+        h3.innerText=e.strMeal
+        h3.addEventListener("click",function(){
+            window.location.href=`./recipe.html?id=${e.idMeal}`
+        })
+        div.append(img,h3)
+        document.getElementById("searched-items").append(div)
+    })
+}
+
+
+let id;
+
+function debounce(func,delay){
+    if(id){
+        clearTimeout(id)
+    }
+    id=setTimeout(()=>{
+        func()
+    },delay)
+}
--- scripts/recipe.js
+++ scripts/recipe.js
@@ -13,16 +13,41 @@ function recipeData(){
         console.log(res.meals);
         res.meals.map((e)=>{
             let div=document.createElement("div")
+
             let h2=document.createElement("h2")
             h2.innerText=e.strMeal
+
             let img=document.createElement("img")
             img.src=e.strMealThumb
+
             let h4=document.createElement("h4")
             h4.innerText=`Category: ${e.strCategory}`
+
+            let origin=document.createElement("h4")
+            origin.innerText=`Origin: ${e.strArea}`
+
             let p2=document.createElement("p")
-            let p3=document.createElement("p")
-            p3.innerText=`Instructions: ${e.strInstructions}`
-            div.append(h2,img,h4)
+            p2.innerText=`Instructions: ${e.strInstructions}`
+
+            let ingr=document.createElement("h3")
+            ingr.innerText="Ingredients"
+
+            let table=document.createElement("table")
+            let thead=document.createElement("thead")
+            let tbody=document.createElement("tbody")
+            for(let i=1;i<=20;i++){
+                let tr=document.createElement("tr")
+                let td1=document.createElement("td")
+                let td2=document.createElement("td")
+                if(`e.strIngredient${i}`){
+                    td1.innerText=`e.strIngredient${i}`
+                    td2.innerText=`e.strMeasure${i}`
+                }
+                tr.append(td1,td2)
+                tbody.append(tr)
+            }
+            table.append(tbody)
+            div.append(img,h2,h4,origin,ingr,p2,table)
             document.getElementById("recipe-card").append(div)
         })
     })
